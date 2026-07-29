60a90200e29a16ecdda0e2422a7446e508092e9b
Mikael <mikaelkwessel@gmail.com>
2026-07-29 10:46:05 +0200

    Prepare NodeCast TV Plus v2.5.1

--- scripts/migration-test.js
+++ scripts/migration-test.js
@@ -29,8 +29,8 @@ const baselines = [
         context: 'https://github.com/technomancer702/nodecast-tv.git#0e26a90dae211cf9ed4c7adc8941ec9fbddec972'
     },
     {
-        version: '2.4.0',
-        image: 'ghcr.io/mikaelkw/nodecast-tv-plus:2.4.0',
+        version: '2.5.0',
+        image: 'ghcr.io/mikaelkw/nodecast-tv-plus:2.5.0',
         kind: 'plus'
     }
 ];
